588a14fc1b2cb1c7992ce6cbf19c829d49189207
KrishnaKhadke <krishnakhadke11@gmail.com>
2023-07-09 01:03:15 +0530

    Files rearrangement & Task_2 : car.py update

---
+++ Task_2/car.py
@@ -0,0 +1,10 @@
+import Task_2.serviceable as serviceable
+
+
+class Car(serviceable):
+    def __init__(self,engine,battery):
+        self.engine = engine
+        self.battery = battery
+        
+    def needs_service(self):
+        return self.engine.needs_service() or self.battery.needs_service()
